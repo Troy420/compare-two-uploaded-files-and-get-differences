79afb4dd23dd38b5f7620961e04b21305cf1a83e
Bernard <bernardy_ho@hotmail.com>
2021-10-22 15:52:34 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let money = 0;
+
+do {
+  if (money <= 1) {
+    console.log("you have " + money + " dollar");
+  } else {
+    console.log("you have " + money + " dollars");
+  }
+  money++;
+} while (money < 10);
